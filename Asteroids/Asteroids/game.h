778ce7b17aacc6c79e74012b0f3b648bb8516de6
(Student) - Andrew Bashorum <c00238900@ITCARLOW.IE>
2019-03-08 12:53:16 +0000

    SfML game loop-heskey

--- Asteroids/Asteroids/game.h
+++ Asteroids/Asteroids/game.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include "MyVector3.h"
+#include <iostream> 
+#include <SFML/Graphics.hpp>
+#include <stdlib.h> 
+#include <time.h> 
+#include "Game.h"
+
+// Andrew Bashorum
+// est time: 40 hours 
+// act time 2 hours, +
+
+class Game
+{
+public:
+	enum GameState { licenseScreen, splashScreen, mainMenu, helpScreen, controlHelp, upgradeHelp, pickUpHelp, Upgrade, level };
+
+	GameState gameState = licenseScreen;
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void update(sf::Time t_deltaTime);
+	void render();
+
+	void setupFontAndText();
+	void setupSprite();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+	sf::Text m_welcomeMessage; // text used for message on screen
+	sf::Texture m_logoTexture; // texture used for sfml logo
+	sf::Sprite m_logoSprite; // sprite used for sfml logo
+	bool m_exitGame; // control exiting game
+
+};
