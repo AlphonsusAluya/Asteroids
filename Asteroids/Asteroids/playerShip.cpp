f4d2c9caef884ca57be80f2ad28603c91ef874cd
(Student) - Andrew Bashorum <c00238900@ITCARLOW.IE>
2019-04-11 10:51:37 +0100

    movemeant actually finished

--- Asteroids/Asteroids/playerShip.cpp
+++ Asteroids/Asteroids/playerShip.cpp
@@ -2,7 +2,7 @@
 
 Player::Player()
 {
-	if (!texture.loadFromFile("ASSETS/IMAGES/spaceship_enemy_red.png"))
+	if (!texture.loadFromFile("ASSETS/IMAGES/MUPMUP7UP.png"))
 	{
 		std::cout << "error with large asteroid image";
 	}
@@ -16,10 +16,7 @@ Player::Player()
 	sprite.setScale(0.15, 0.15);
 	
 	location = { 200,300,0 };
-	
-	
 	sprite2.setTexture(icon);
-	
 }
 
 void Player::update()
@@ -50,37 +47,34 @@ sf::Sprite Player::getBody2()
 
 void Player::rotate(sf::Event t_event)
 {
-
+	MyVector3 force = { 0,0,0 };
 	lookDirection.x = cos(angle);
 	lookDirection.y = sin(angle);
 	
 	sprite.setRotation(angle * (180 / 3.14159265359));
 
-	velocity = lookDirection;
-	// get keyboard input.
+	
 
-
-	// get keyboard input.
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
-		angle -= 0.25;
-
+		angle -= 0.2;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
-		angle += 0.25;
+		angle += 0.2;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 	{
-		if (speed < MAX_FORWARD_SPEED)
-		{
-			speed += acceleration;
-		}
+		force = force + lookDirection;
+		//if (speed < MAX_FORWARD_SPEED)
+		//{
+		//	speed += acceleration;
+		//}
 	}
-
-
-	velocity.y *= speed;
-	velocity.x *= speed;
+	
+	//velocity.y *= speed;
+	//velocity.x *= speed;
+	velocity = velocity + force;
 }
 
 
@@ -88,22 +82,22 @@ void Player::checkBorders()
 {
 	if (location.x > 800)
 	{
-		location.x = location.x - 1080;
+		location.x = location.x - 840;
 	}
 
 	if (location.x < 0 - REAL_SIZE)
 	{
-		location.x = location.x + 1080;
+		location.x = location.x + 840;
 	}
 
 	if (location.y >= 600)
 	{
-		location.y = location.y - 880;
+		location.y = location.y - 840;
 	}
 
 	if (location.y < 0 -REAL_SIZE)
 	{
-		location.y = location.y + 880;
+		location.y = location.y + 840;
 	}
 }
 
